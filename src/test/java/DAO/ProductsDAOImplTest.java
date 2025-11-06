f79934765c33ac9fc568e3ac00164ef4592f2e7c
amitha205012 <amithaperiyasamy2005@gmail.com>
2025-11-06 19:52:49 +0530

    ProductsDAOImplTest

---
+++ src/test/java/DAO/ProductsDAOImplTest.java
@@ -0,0 +1,45 @@
+package dao;
+
+import model.Product;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsDAOImplTest {
+
+    @Mock
+    Connection mockConnection;
+
+    @Mock
+    PreparedStatement mockStatement;
+
+    @Mock
+    ResultSet mockResultSet;
+
+    @InjectMocks
+    ProductsDAOImpl dao;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        dao = new ProductsDAOImpl(mockConnection);
+    }
+
+    @Test
+    void testAddProduct() throws Exception {
+        Product p = new Product(0, "Laptop", "Gaming Laptop", 1200.0, 10, 5);
+        when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockStatement);
+        when(mockStatement.executeUpdate()).thenReturn(1);
+
+        int id = dao.addProduct(p);
+        assertTrue(id > 0);
+
+        verify(mockStatement, times(1)).executeUpdate();
+    }
+}
